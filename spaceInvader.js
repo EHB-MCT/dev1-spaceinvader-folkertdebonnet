4064dfa6b7446056be44a31d03ea0b66639bcb6a
folkert debonnet <folkert.debonnet@student.ehb.be>
2022-10-06 20:11:13 +0200

    drew an spaceinvader profile pic

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,25 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+drawspaceinvader();
+
+function drawspaceinvader(){
+    context.beginPath();
+    context.rect(0,0,300,300);
+    context.fill();
+
+    context.beginPath();
+    context.rect(25,25,250,50);
+    context.rect(25,75,50,50);
+    context.rect(225,75,50,50);
+    context.rect(75,125,150,50);
+    context.rect(25,175,50,100);
+    context.rect(125,175,50,100);
+    context.rect(225,175,50,100);
+    context.fillStyle = "#00ff00";
+    context.fill();
+}
